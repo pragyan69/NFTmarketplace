a0c6898f44a75bff6cd0b3d7c258c4d9a2c89399
pragyan69 <104765708+pragyan69@users.noreply.github.com>
2023-04-14 08:02:55 +0000

    third commit

--- frontend/my-app/pages/_app.js
+++ frontend/my-app/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
+const   App=({ Component, pageProps }) =>{
   return <Component {...pageProps} />
 }
+export default App
